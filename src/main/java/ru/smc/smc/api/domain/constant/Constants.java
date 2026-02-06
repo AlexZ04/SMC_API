58af6b6ef8a33fcd82cf9ad406641a16ff06338d
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-02-06 18:22:11 +0700

    Добавлена возможность поменять состояние тоггла

---
+++ src/main/java/ru/smc/smc/api/domain/constant/Constants.java
@@ -0,0 +1,5 @@
+package ru.smc.smc.api.domain.constant;
+
+public class Constants {
+    public static final String TOGGLE_FUNCTIONAL = "Функциональность данного тоггла уже ";
+}
--- src/main/java/ru/smc/smc/api/service/FeatureToggleService.java
+++ src/main/java/ru/smc/smc/api/service/FeatureToggleService.java
@@ -2,6 +2,8 @@ package ru.smc.smc.api.service;
 
 import com.sun.tools.javac.Main;
 import org.springframework.stereotype.Service;
+import ru.smc.smc.api.domain.constant.Constants;
+import ru.smc.smc.api.domain.exceptions.BadRequestException;
 import ru.smc.smc.api.domain.model.toggle.FeatureToggle;
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
@@ -10,6 +12,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FeatureToggleService {
@@ -19,6 +22,31 @@ public class FeatureToggleService {
             Path.of("config/feature-toggles.json");
 
     public String getSystemTogglesInfo() {
+        List<FeatureToggle> toggles = getAllToggles();
+
+        return parseToggleListToText(toggles);
+    }
+
+    public void changeActiveToggleStatus(String toggleName, boolean activate) {
+        Optional<FeatureToggle> toggleOptional = getSpecificToggle(toggleName);
+
+        if (toggleOptional.isPresent()) {
+            FeatureToggle toggle = toggleOptional.get();
+
+            if (toggle.isActive() != activate) {
+                toggle.setActive(activate);
+            }
+            else {
+                throw new BadRequestException(Constants.TOGGLE_FUNCTIONAL +
+                        (activate ? "включена: " : "выключена: ") +
+                        toggle.getToggleName());
+            }
+        } else {
+            throw new BadRequestException("Тоггл " + toggleName + " не найден");
+        }
+    }
+
+    private List<FeatureToggle> getAllToggles() {
         List<FeatureToggle> toggles;
 
         if (Files.exists(TOGGLES_PATH)) {
@@ -37,7 +65,13 @@ public class FeatureToggleService {
             );
         }
 
-        return parseToggleListToText(toggles);
+        return toggles;
+    }
+
+    private Optional<FeatureToggle> getSpecificToggle(String toggleName) {
+        return getAllToggles().stream()
+                .filter(toggle -> toggle.getToggleName().equalsIgnoreCase(toggleName))
+                .findFirst();
     }
 
     private String parseToggleListToText(List<FeatureToggle> toggles) {

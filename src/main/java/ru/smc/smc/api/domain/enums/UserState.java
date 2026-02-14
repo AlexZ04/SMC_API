63421d3243862b27a05a4831818d62a5736e638b
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-02-14 13:02:41 +0700

    Enum-класс UserState расширен для взаимодействия с UI-инструментом

--- src/main/java/ru/smc/smc/api/domain/enums/UserState.java
+++ src/main/java/ru/smc/smc/api/domain/enums/UserState.java
@@ -1,5 +1,15 @@
 package ru.smc.smc.api.domain.enums;
 
 public enum UserState {
-    MAIN_MENU
+    MAIN_MENU(0);
+
+    private final int keyboardCode;
+
+    UserState(int keyboardCode) {
+        this.keyboardCode = keyboardCode;
+    }
+
+    public int getCode() {
+        return keyboardCode;
+    }
 }
